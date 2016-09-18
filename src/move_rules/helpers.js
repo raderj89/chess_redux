5a96f938c29a61621b1a265aa4ce439ad33c461a
Connor Harwood <harwood.connor@gmail.com>
2016-09-18 14:47:35 -0700

    add rook move fn, inBounds helper

---
+++ src/move_rules/helpers.js
@@ -0,0 +1,3 @@
+export const inBounds([x, y]) => {
+  return x < 8 && x > -1 && y < 8 && y > -1;
+};
---
+++ src/move_rules/rook.js
@@ -0,0 +1,55 @@
+import {inBounds} from './helpers';
+
+export const rook = (position, board) => {
+  const moves = [];
+  const {color} = board[position[0]][position[1]];
+  let canUp = true;
+  let canDown = true;
+  let canLeft = true;
+  let canRight = true;
+
+  for(let i = 1, i < 8; i++) {
+    let up = [position[0] + i, position[1]];
+    let down = [position[0] - i, position[1]];
+    let left = [position[0], position[1] - i];
+    let right = [position[0], position[1] + i];
+    let target;
+
+    if(canUp && inBounds(up)) {
+      target = board[up[0], up[1]];
+      if(target.color) {
+        canUp = false;
+      }
+      if(target.color != color) {
+        moves.push(up);
+      }
+    }
+    if(canDown && inBounds(down)) {
+      target = board[down[0], down[1]];
+      if(target.color) {
+        canDown = false;
+      }
+      if(target.color != color) {
+        moves.push(down);
+      }
+    }
+    if(canLeft && inBounds(left)) {
+      target = board[left[0], left[1]];
+      if(target.color) {
+        canLeft = false;
+      }
+      if(target.color != color) {
+        moves.push(left);
+      }
+    }
+    if(canRight && inBounds(right)) {
+      target = board[right[0], right[1]];
+      if(target.color) {
+        canRight = false;
+      }
+      if(target.color != color) {
+        moves.push(right);
+      }
+    }
+  }
+};
